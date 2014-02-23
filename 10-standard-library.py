d96b33b3be966faeeef4bdc38c0c4030c85bb6ad
Jonathan Chen (home) <chen@chinstruments.com>
2014-02-23 09:36:35 -0600

    chapter 11 was a bit slower, but still a breeze since it's more 'learning about' than 'learning'

--- 10-standard-library.py
+++ 10-standard-library.py
@@ -19,3 +19,17 @@ print '''
 Section 10.11: Quality Control
 - import doctest
 - doctest.testmod() # run tests and checks vs answers EMBEDDED IN DOCSTRINGS!!!
+'''
+
+print '''
+Section 11.1: Output Formatting
+- pprint.pprint(): pretty printer
+'''
+
+print '''
+Section 11.2: Tools for Working with Lists
+- array.array(): C-style array with homogeneous data [can specify less than the usual 16 bytes (!!) per entry for regular lists of Python int objects]
+- collections.deque():  fast append/pop from left, slower lookup in middle
+- "bisect" module for manipulating sorted lists
+- "heapq" module for implementing heaps based on regular lists
+'''
